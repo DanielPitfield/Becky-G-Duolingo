05c06aaf3bbe2a629de2dd743f3fb81b8665d521
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-29 06:17:32 +0000

    refactor container check

--- src/script.ts
+++ src/script.ts
@@ -10,7 +10,7 @@ targetClassNames.forEach((className) => {
 // Returns an image element (of a random Becky G image)
 function createImage(): HTMLImageElement {
   const image: HTMLImageElement = document.createElement("img");
-  
+
   image.src = getRandomBeckyGImageURL();
   image.width = 150;
   image.height = 150;
@@ -21,27 +21,30 @@ function createImage(): HTMLImageElement {
 
 function replaceImages() {
   // Get all the elements with the target classNames
-  const cartoonImages: NodeListOf<Element> = document.querySelectorAll(
+  const targetImages: NodeListOf<Element> = document.querySelectorAll(
     targetClassNames.map((name) => `.${name}`).join(",")
   );
 
   // Only the image elements which haven't already been replaced
-  const cartoonImagesFiltered: Element[] = Array.from(cartoonImages).filter(
+  const targetImagesFiltered: Element[] = Array.from(targetImages).filter(
     (image) => image.getAttribute("data-is-image-replaced") === null
   );
 
-  cartoonImagesFiltered.forEach((image) => {
-    if (image.parentElement) {
-      // Keep and add to the styling of the container
-      image.parentElement.style.cssText +=
-        "display: flex; align-items: center; justify-content: center;";
-
-      // Replace old image element with new image element
-      const newImage = createImage();
-      image.parentElement.replaceChild(newImage, image);
-      newImage.setAttribute("data-is-image-replaced", "true");
+  for (const image of targetImagesFiltered) {
+    // Don't replace the image, if there is no container
+    if (image.parentElement === null) {
+      continue;
     }
-  });
+
+    // Keep and add to the styling of the container
+    image.parentElement.style.cssText +=
+      "display: flex; align-items: center; justify-content: center;";
+
+    // Replace old image element with new image element
+    const newImage = createImage();
+    image.parentElement.replaceChild(newImage, image);
+    newImage.setAttribute("data-is-image-replaced", "true");
+  }
 }
 
 // Periodically look for any image elements that need replacing
