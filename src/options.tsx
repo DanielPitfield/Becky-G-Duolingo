aad25877675183b7d8bcf26d93e6a1ed11143d0d
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-29 02:04:10 +0000

    options (initial)

---
+++ src/options.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Options = () => {
+  return <></>;
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Options />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
