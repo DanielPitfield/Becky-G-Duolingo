d2912e89fad916dc3156755544bf0d983e8340da
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-29 05:24:00 +0000

    more images

--- src/data.ts
+++ src/data.ts
@@ -1,6 +1,12 @@
 const beckyImages: string[] = [
   chrome.runtime.getURL("images/becky1.jpg"),
   chrome.runtime.getURL("images/becky2.jpg"),
+  chrome.runtime.getURL("images/becky3.jpg"),
+  chrome.runtime.getURL("images/becky4.jpg"),
+  chrome.runtime.getURL("images/becky5.jpg"),
+  chrome.runtime.getURL("images/becky6.jpg"),
+  chrome.runtime.getURL("images/becky7.jpg"),
+  chrome.runtime.getURL("images/becky8.jpg"),
 ];
 
 export function getRandomBeckyGImageURL(): string {
