f8a9d9bba2a083083323966d8a0a92885850b189
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-29 02:16:42 +0000

    refactor content script

---
+++ src/data.ts
@@ -0,0 +1,19 @@
+export const beckyImages = [
+  "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRmdUSBTYpAWbhsIYZB2ljhdlWmaBX-7bcUGqmf_Ri0Yhg_5Kucfm4YEDhiehMNJFf4qmPAvh1JKDQaA_w",
+  "https://media1.popsugar-assets.com/files/thumbor/j2df4DKTYt_7evD8J3UEvDnZJkA/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2022/08/28/041/n/1922153/9bceaaaf612a51fc_GettyImages-1418876873/i/becky-g-makeup-mtv-vmas.jpg",
+  "https://m.media-amazon.com/images/M/MV5BMTUzMjg0MjgxMF5BMl5BanBnXkFtZTgwMzkyNDM5NjE@._V1_.jpg",
+  "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/df912aff-284c-4d0d-971b-e45d026ef6da/180_FLAUNT_MAGAZINE_FLAUNT.COM_WEW_COVER_BECKY_G4.jpg?format=1000w",
+];
+
+export const cartoonImageClassNames = [
+  // Right side on path of level
+  "_3a8EI._1cRje",
+  // Left side on path of level
+  "_1cRje",
+  // Questions
+  "F2B9m > div",
+  // Loading level
+  "_3a8EI._1S4zC",
+  // Review reminders
+  "_3a8EI._2IFQ2",
+];
--- src/script.ts
+++ src/script.ts
@@ -1,49 +1,44 @@
-const beckyImages = [
-  "http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcRmdUSBTYpAWbhsIYZB2ljhdlWmaBX-7bcUGqmf_Ri0Yhg_5Kucfm4YEDhiehMNJFf4qmPAvh1JKDQaA_w",
-  "https://media1.popsugar-assets.com/files/thumbor/j2df4DKTYt_7evD8J3UEvDnZJkA/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2022/08/28/041/n/1922153/9bceaaaf612a51fc_GettyImages-1418876873/i/becky-g-makeup-mtv-vmas.jpg",
-  "https://m.media-amazon.com/images/M/MV5BMTUzMjg0MjgxMF5BMl5BanBnXkFtZTgwMzkyNDM5NjE@._V1_.jpg",
-  "https://images.squarespace-cdn.com/content/v1/56c346b607eaa09d9189a870/df912aff-284c-4d0d-971b-e45d026ef6da/180_FLAUNT_MAGAZINE_FLAUNT.COM_WEW_COVER_BECKY_G4.jpg?format=1000w",
-];
+import { cartoonImageClassNames, beckyImages } from "./data";
 
-const cartoonImageClassNames = [
-  // Right side on path of level
-  "_3a8EI._1cRje",
-  // Left side on path of level
-  "_1cRje",
-  // Questions
-  "F2B9m > div",
-  // Loading level
-  "_3a8EI._1S4zC",
-  // Review reminders
-  "_3a8EI._2IFQ2",
-];
-
+// Hide all images that have the target classNames (until they are replaced)
 cartoonImageClassNames.forEach((className) => {
   const styleElement = document.createElement("style");
   styleElement.textContent = `.${className}:not([data-is-image-replaced="true"]) { visibility: hidden; }`;
   document.head.appendChild(styleElement);
 });
 
-setInterval(() => {
+function replaceImages() {
+  // Get all the elements with the target classNames
   const cartoonImages = document.querySelectorAll(
     cartoonImageClassNames.map((name) => `.${name}`).join(",")
   );
 
-  Array.from(cartoonImages)
-    .filter((image) => image.getAttribute("data-is-image-replaced") === null)
-    .forEach((image) => {
-      const newImage = document.createElement("img");
-      newImage.src =
-        beckyImages[Math.floor(Math.random() * beckyImages.length)];
-      newImage.width = 150;
-      newImage.height = 150;
-      newImage.style.borderRadius = "4em";
+  // Only the image elements which haven't already been replaced
+  const cartoonImagesFiltered = Array.from(cartoonImages).filter(
+    (image) => image.getAttribute("data-is-image-replaced") === null
+  );
 
-      if (image.parentElement) {
-        newImage.setAttribute("data-is-image-replaced", "true");
-        image.parentElement.style.cssText +=
-          "display: flex; align-items: center; justify-content: center;";
-        image.parentElement.replaceChild(newImage, image);
-      }
-    });
-}, 100);
+  cartoonImagesFiltered.forEach((image) => {
+    // Create an image element
+    const newImage = document.createElement("img");   
+    // Using a random Becky G image 
+    newImage.src = beckyImages[Math.floor(Math.random() * beckyImages.length)];
+    // Style the image
+    newImage.width = 150;
+    newImage.height = 150;
+    newImage.style.borderRadius = "4em";
+
+    // Keep the container styling
+    if (image.parentElement) {
+      image.parentElement.style.cssText +=
+        "display: flex; align-items: center; justify-content: center;";
+      // Replace old image element with new image element
+      image.parentElement.replaceChild(newImage, image);
+      newImage.setAttribute("data-is-image-replaced", "true");
+    }
+  });
+}
+
+// Periodically look for any image elements that need replacing
+const CHECK_INTERVAL_MS = 100;
+setInterval(replaceImages, CHECK_INTERVAL_MS);
