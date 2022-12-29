a61c191f078baeb22222bc60324d8be9915e73c4
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-29 06:08:53 +0000

    decouple creating new image

--- src/script.ts
+++ src/script.ts
@@ -7,6 +7,18 @@ targetClassNames.forEach((className) => {
   document.head.appendChild(styleElement);
 });
 
+// Returns an image element (of a random Becky G image)
+function createImage(): HTMLImageElement {
+  const image: HTMLImageElement = document.createElement("img");
+  
+  image.src = getRandomBeckyGImageURL();
+  image.width = 150;
+  image.height = 150;
+  image.style.borderRadius = "4em";
+
+  return image;
+}
+
 function replaceImages() {
   // Get all the elements with the target classNames
   const cartoonImages: NodeListOf<Element> = document.querySelectorAll(
@@ -19,21 +31,13 @@ function replaceImages() {
   );
 
   cartoonImagesFiltered.forEach((image) => {
-    // Create an image element
-    const newImage: HTMLImageElement = document.createElement("img");
-    // Using a random Becky G image
-    newImage.src = getRandomBeckyGImageURL();
-    // Style the image
-    newImage.width = 150;
-    newImage.height = 150;
-    newImage.style.borderRadius = "4em";
-
-    // If the image being replaced had a container
     if (image.parentElement) {
-      // Keep and add to that styling
+      // Keep and add to the styling of the container
       image.parentElement.style.cssText +=
         "display: flex; align-items: center; justify-content: center;";
+
       // Replace old image element with new image element
+      const newImage = createImage();
       image.parentElement.replaceChild(newImage, image);
       newImage.setAttribute("data-is-image-replaced", "true");
     }
