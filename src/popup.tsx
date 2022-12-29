dfc517f5ee9b847ed87bab01a01a42b1ffb8a263
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-29 02:00:49 +0000

    react popup

---
+++ src/popup.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Popup = () => {
+  return <div>Replace cartoony images on Duolingo with Becky G!</div>;
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Popup />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
