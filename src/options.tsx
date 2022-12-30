66d4acd1498a3b1dc57b9cd62caa2e1e5b87ea49
Daniel Pitfield <danielpitfield1@gmail.com>
2022-12-30 18:11:16 +0000

    saved options (initial)

--- src/options.tsx
+++ src/options.tsx
@@ -1,16 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import { targetSelectors } from "./data";
 
 const Options = () => {
+  // All target selectors are enabled by default
+  const defaultTargetSelectorOptions = targetSelectors.map((x) => {
+    return { selectorLabel: x.label, isEnabled: true};
+  });
+
+  const [targetSelectorOptions, setTargetSelectorOptions] = useState<
+    { selectorLabel: string; isEnabled: boolean }[]
+  >(defaultTargetSelectorOptions);
+
+  const [status, setStatus] = useState<string>("");
+
+  // Restores the state of the selected options (using the preferences stored in chrome.storage)
+  useEffect(() => {
+    chrome.storage.sync.get("targetSelectorOptions", (item) => {
+      setTargetSelectorOptions(item.targetSelectorOptions);
+    });
+  }, []);
+
+  // Saves options to chrome.storage
+  function saveOptions() {
+    chrome.storage.sync.set({ targetSelectorOptions }, () => {
+      // Update status to let user know options were saved.
+      setStatus("Options saved.");
+
+      // Clear status message
+      const id = setTimeout(() => {
+        setStatus("");
+      }, 1000);
+
+      return () => clearTimeout(id);
+    });
+  }
+
   return (
-    <div className="options">
-      {targetSelectors.map(({ label }) => (
-        <label key={label} className="option">
-          {label}
-          <input type="checkbox" className="option-checkbox" />
-        </label>
-      ))}
+    <div>
+      <div className="options">
+        {targetSelectors.map(({ label }) => (
+          <label key={label} className="option">
+            {label}
+            <input
+              type="checkbox"
+              className="option-checkbox"
+              checked={targetSelectorOptions?.some(
+                (option) => option.selectorLabel === label && option.isEnabled
+              )}
+              onChange={() => {
+                // Find the option being changed in targetSelectorOptions
+                const changedOption = targetSelectorOptions.find(
+                  (option) => option.selectorLabel === label
+                );
+
+                // Toggle enabled status
+                if (changedOption) {
+                  changedOption.isEnabled = !changedOption.isEnabled;
+                }
+              }}
+            />
+          </label>
+        ))}
+      </div>
+
+      <div>{status}</div>    
+      <button onClick={saveOptions}>Save</button>
     </div>
   );
 };
