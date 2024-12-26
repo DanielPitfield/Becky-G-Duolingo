94b2033da2aa9da2c29365aee8469de2ae6764aa
Daniel Pitfield <danielpitfield1@gmail.com>
2024-12-26 11:17:43 +0000

    Refactor background script

--- src/script.ts
+++ src/script.ts
@@ -1,44 +1,34 @@
 import { getEnabledTargetSelectors, createImage } from "./utils";
 
-const selectors = await (async (): Promise<string[]> => {
-  const targetSelectorOptions = await getEnabledTargetSelectors();
-  return targetSelectorOptions.flatMap(({ selectors }) => selectors);
-})();
+const selectors = (await getEnabledTargetSelectors()).flatMap(({ selectors }) => selectors);
+const CHECK_INTERVAL_MS = 100;
 
-(async () => {
-  // Hide all image elements that match an enabled CSS selector (until they are replaced)
-  selectors.forEach((selector) => {
-    const styleElement = document.createElement("style");
-    styleElement.textContent = `${selector}:not([data-is-image-replaced="true"]) { visibility: hidden; }`;
-    document.head.appendChild(styleElement);
-  });
-})();
+// Periodically look for any image elements that need replacing
+setInterval(replaceImages, CHECK_INTERVAL_MS);
+
+// Hide all image elements that match an enabled CSS selector (until they are replaced)
+selectors.forEach((selector) => {
+  const styleElement = document.createElement("style");
+  styleElement.textContent = `${selector}:not([data-is-image-replaced="true"]) { visibility: hidden; }`;
+  document.head.appendChild(styleElement);
+});
 
 async function replaceImages() {
   // Get all the elements that match an enabled CSS selector
   const targetImages: NodeListOf<Element> = document.querySelectorAll(selectors.join(","));
 
-  // Only the image elements which haven't already been replaced
+  // Only the image elements which haven't already been replaced and have a container
   const filteredTargetImages: Element[] = Array.from(targetImages).filter(
-    (image) => !image.getAttribute("data-is-image-replaced")
+    (image) => !image.getAttribute("data-is-image-replaced") && image.parentElement
   );
 
-  for (const image of filteredTargetImages) {
-    // Don't replace the image, if there is no container
-    if (!image.parentElement) {
-      continue;
-    }
-
+  filteredTargetImages.forEach((image) => {
     // Keep and add to the styling of the container
-    image.parentElement.style.cssText += "display: flex; align-items: center; justify-content: center;";
+    image.parentElement!.style.cssText += "display: flex; align-items: center; justify-content: center;";
 
     // Replace old image element with new image element
     const newImage = createImage();
-    image.parentElement.replaceChild(newImage, image);
+    image.parentElement!.replaceChild(newImage, image);
     newImage.setAttribute("data-is-image-replaced", "true");
-  }
+  });
 }
-
-// Periodically look for any image elements that need replacing
-const CHECK_INTERVAL_MS = 100;
-setInterval(replaceImages, CHECK_INTERVAL_MS);
